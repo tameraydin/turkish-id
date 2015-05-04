f78fe38d1fc2f1ae97fcf7be7f46af5682089f85
Tamer Aydin <tamer.aydin@gmail.com>
2015-05-04 02:22:38 +0200

    clean-up

--- test/main.spec.js
+++ test/main.spec.js
@@ -6,29 +6,26 @@ var exampleID;
 
 describe('turkish-id', function() {
 
-  describe('itself', function() {
+  it('should be able to generate', function() {
+    exampleID = turkishId.generate();
 
-    it('should be able to generate', function() {
-      exampleID = turkishId.generate();
+    expect(typeof exampleID).toBe('number');
+    expect((exampleID + '').length).toBe(11);
+  });
 
-      expect(typeof exampleID).toBe('number');
-      expect((exampleID + '').length).toBe(11);
-    });
+  it('should validate properly', function() {
+    expect(turkishId.validate(null)).toBe(false);
+    expect(turkishId.validate(01111111111)).toBe(false);
+    expect(turkishId.validate((exampleID + '').substr(0, 9) + 'x1')).toBe(false);
+    expect(turkishId.validate(exampleID + 1)).toBe(false);
 
-    it('should validate properly', function() {
-      expect(turkishId.validate(null)).toBe(false);
-      expect(turkishId.validate(01111111111)).toBe(false);
-      expect(turkishId.validate((exampleID + '').substr(0, 9) + 'x1')).toBe(false);
-      expect(turkishId.validate(exampleID + 1)).toBe(false);
+    expect(turkishId.validate(exampleID)).toBe(true);
+    expect(turkishId.validate(exampleID + '')).toBe(true);
+  });
 
-      expect(turkishId.validate(exampleID)).toBe(true);
-      expect(turkishId.validate(exampleID + '')).toBe(true);
-    });
-
-    it('should be consistent', function() {
-      for (var i = 0; i < 10; i++) {
-        expect(turkishId.validate(turkishId.generate())).toBe(true);
-      }
-    });
+  it('should be consistent', function() {
+    for (var i = 0; i < 10; i++) {
+      expect(turkishId.validate(turkishId.generate())).toBe(true);
+    }
   });
 });
